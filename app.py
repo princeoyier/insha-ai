2ce4edc2e86e52f295cac8ff2ae6e715bc0f714a
princeoyier <167521882+princeoyier@users.noreply.github.com>
2026-02-20 02:10:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -36,4 +36,7 @@ def index():
     return render_template("index.html", feedback=feedback)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
